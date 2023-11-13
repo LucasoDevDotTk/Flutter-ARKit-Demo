dc9b9f7afee90d875dd6934ec7f8b9841bd22ee7
Lucas Nguyen <lucasoAlt@outlook.com>
2023-11-13 19:55:54 +0100

    first real commit lol

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:arkit_plugin/arkit_plugin.dart';
+import 'package:vector_math/vector_math_64.dart';
 
 void main() {
   runApp(MyApp());
@@ -71,15 +73,32 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class CameraPage extends StatelessWidget {
+class CameraPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text('Camera Page'),
-    );
-  }
+  _CameraPage createState() => _CameraPage();
 }
 
+class _CameraPage extends State<CameraPage> {
+  late ARKitController arkitController;
+
+  @override
+  void dispose() {
+    arkitController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+      appBar: AppBar(title: const Text('ARKit in Flutter')),
+      body: ARKitSceneView(onARKitViewCreated: onARKitViewCreated));
+
+  void onARKitViewCreated(ARKitController arkitController) {
+    this.arkitController = arkitController;
+    final node = ARKitNode(
+        geometry: ARKitSphere(radius: 0.1), position: Vector3(0, 0, -0.5));
+    this.arkitController.add(node);
+  }
+}
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
